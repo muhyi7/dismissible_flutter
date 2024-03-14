04fe38e8dd5697af08eaf3000c965abe8aa78854
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-03-14 21:02:58 +0700

    dengan konfirmasi hapus

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,15 +27,56 @@ class MyHomePage extends StatelessWidget {
         itemCount: 100,
         itemBuilder: (context, index) {
           return Dismissible(
+            onDismissed: (direction) {
+              print("DISMISSED");
+              print(direction);
+              if (direction == DismissDirection.endToStart) {
+                print("END TO START");
+              } else {
+                print("START TO END");
+              }
+            },
+            confirmDismiss: (direction) {
+              return showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text("Confirm"),
+                      content: Text("Are you sure delete this item ?"),
+                      actions: [
+                        TextButton(onPressed: () {
+                          // print("No");
+                          Navigator.of(context).pop(false);
+                        }, child: Text("No")),
+                        TextButton(onPressed: () {
+                          // print("Yes");
+                          Navigator.of(context).pop(true);
+                        }, child: Text("No"))
+                      ],
+                    );
+                  });
+            },
             key: Key(index.toString()),
+            // direction: DismissDirection.endToStart,
+            background: Container(
+              color: Colors.red,
+              child: Icon(
+                Icons.delete,
+                size: 25,
+              ),
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.only(right: 10),
+            ),
             child: ListTile(
-              leading: CircleAvatar(),
+              leading: CircleAvatar(
+                child: Center(child: Text("${index + 1}")),
+              ),
               title: Text(faker.person.name()),
               subtitle: Text(faker.lorem.sentence()),
             ),
           );
-      },),
+        },
+      ),
     );
   }
 }
-
